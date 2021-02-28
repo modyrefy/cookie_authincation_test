bb929dadc2114c1d5fd34bf1b5411f0d3c86a037
modyrefy <modyrefay@gmail.com>
2021-02-28 17:40:25 +0400

    update UserAuthincateResponse

--- cookie_authincation_test/UserAuthincateResponse.cs
+++ cookie_authincation_test/UserAuthincateResponse.cs
@@ -11,5 +11,6 @@ namespace cookie_authincation_test
         public string UserName { get; set; }
         public string EmailAddress { get; set; }
         public DateTime CreationDate { get; set; }
+        public string Token { get; set; }
     }
 }
